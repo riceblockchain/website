e878505e3bcce915f870864eda12519b4e153feb
yorhodes <yorkerhodesiv@gmail.com>
2019-01-16 22:51:42 -0600

    deploy new build

--- docs/_next/static/development/pages/index.js
+++ docs/_next/static/development/pages/index.js
@@ -13,7 +13,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 var _jsxFileName = "/mnt/c/Users/yorke/dev/riceblockchain/website/components/favicons.js";
 
-var favIconPath = "/static/favicon/";
+var favIconPath = "/static/favicons/";
 /* harmony default export */ __webpack_exports__["default"] = (function () {
   return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("link", {
     rel: "apple-touch-icon",
--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a11142762bb99548adfc";
+/******/ 	var hotCurrentHash = "44d689abe4773ff64c46";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
